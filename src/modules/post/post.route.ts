0e42d857de88d8d5e3c484e903b201ffd96c1ff2
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-10-02 12:24:48 +0600

    post task completed

--- src/modules/post/post.route.ts
+++ src/modules/post/post.route.ts
@@ -3,6 +3,13 @@ import { PostController } from "./post.controller";
 
 const route = Router();
 
+route.get("/", PostController.getAllPosts);
+route.get("/:id", PostController.getPostById);
+
 route.post("/", PostController.postCreate);
+
+route.patch("/:id", PostController.updatePost);
+
+route.delete("/:id", PostController.deletePost);
 
 export const postRouter = route;
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -6,4 +6,23 @@ const postCreate = async (payload: Prisma.PostCreateInput): Promise<Post> => {
   return post;
 };
 
-export const PostService = { postCreate };
+const getAllPosts = async () => {
+  const result = await prisma.post.findMany();
+  return result;
+};
+
+const getPostById = async (id: number) => {
+  const result = await prisma.post.findUnique({ where: { id }, include: { author: true } });
+
+  return result;
+};
+
+const updatePost = async (id: number, data: Partial<any>) => {
+  return prisma.post.update({ where: { id }, data });
+};
+
+const deletePost = async (id: number) => {
+  return prisma.post.delete({ where: { id } });
+};
+
+export const PostService = { postCreate, getAllPosts, getPostById, updatePost, deletePost };
